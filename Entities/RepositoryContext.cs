dcd2e04a2d4f25f1991dc99bd82ba4cb43221c7a
周 <zqyo2000z@hotmail.com>
2020-02-12 20:30:30 +0800

    1.项目基础搭建

---
+++ Entities/RepositoryContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entities
+{
+  public  class RepositoryContext:DbContext
+    {
+        public RepositoryContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+    }
+}
