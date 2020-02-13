3a205955e661377a5298b8e6d62bd0eb43f356d8
周 <zqyo2000z@hotmail.com>
2020-02-13 22:23:30 +0800

    新增表

---
+++ Entities/Models/Circuit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Models
+{
+    /// <summary>
+    /// 线路
+    /// </summary>
+   public  class Circuit:IEntity
+    {
+        public int Id { get; set; }
+
+        public string DomainName { get; set; }
+        public bool IsDefault { get; set; }
+        public string Name { get; set; }
+        public int Port { get; set; }
+        public int Type { get; set; }
+        public bool State { get; set; }
+
+    }
+}
---
+++ Entities/Models/Log.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Models
+{
+    /// <summary>
+    /// 日志
+    /// </summary>
+  public   class Log:IEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Description { get; set; }
+        public string Ip { get; set; }
+        /// <summary>
+        /// 位置
+        /// </summary>
+        public string Location { get; set; }
+        public int UserId { get; set; }
+
+
+    }
+}
