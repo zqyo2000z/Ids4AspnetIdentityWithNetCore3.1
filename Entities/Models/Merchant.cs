9186884a78980f1dbbc92c919b43eb124f2312e3
周 <zqyo2000z@hotmail.com>
2020-02-14 15:54:16 +0800

    1.新增部分实体类

---
+++ Entities/Models/Merchant.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Entities.Models
+{
+    public class Merchant : IEntity
+    {
+        public int Id { get; set; }
+
+        public string Account { get; set; }
+        public decimal Fee { get; set; }
+        public DateTime FinalDate { get; set; }
+        public bool IsIps { get; set; }
+        public bool IsLogger { get; set; }
+        public DateTime JoinDate { get; set; }
+        public bool LeaveState { get; set; }
+        public string Nickname { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool PhoneState { get; set; }
+        public string QqNumber { get; set; }
+        public string SecretKey { get; set; }
+        public string ServicePhone { get; set; }
+        public string ServiceQq { get; set; }
+        public bool ServiceState { get; set; }
+        public int SettlementType { get; set; }
+        public int State { get; set; }
+        public bool Type { get; set; }
+        public string Uuid { get; set; }
+        public int ParentId { get; set; }
+        public int RankId { get; set; }
+    }
+}
---
+++ Entities/Models/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Models
+{
+  public   class Message:IEntity
+    {
+        public int Id { get; set; }
+
+        public string Account { get; set; }
+        public string Content { get; set; }
+        public DateTime LeaveDate { get; set; }
+        public string PartitionName { get; set; }
+        public string Phone { get; set; }
+        public string Qq { get; set; }
+        public bool State { get; set; }
+        public int MerchantId { get; set; }
+    }
+}
