0097678c41a6eb97b7b678bf4bcbd8f2b00387d4
gaoshuai <834517120@qq.com>
2020-02-15 15:01:00 +0800

    add something settings

--- ManageWeb/src/main.js
+++ ManageWeb/src/main.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import './assets/css/common.scss';
 Vue.config.productionTip = false
 
 new Vue({
