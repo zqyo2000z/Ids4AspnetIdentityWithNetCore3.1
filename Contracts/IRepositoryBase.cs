7592c6e9c01a4690d5bd6a70d8b4c9071e233390
周 <zqyo2000z@hotmail.com>
2020-02-23 00:31:41 +0800

    1.更新部分基础数据操作

--- Contracts/IRepositoryBase.cs
+++ Contracts/IRepositoryBase.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 
 namespace Contracts
 {
@@ -10,8 +9,136 @@ namespace Contracts
     {
         IQueryable<T> FindAll();
         IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression);
-        void Create(T entity);
+       // void Create(T entity);
         void Update(T entity);
         void Delete(T entity);
+
+        #region 插入数据
+        /// <summary>
+        /// 插入数据
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        void Insert(T t);
+        #endregion
+
+        #region 批量插入数据
+        /// <summary>
+        /// 批量插入数据
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        void Insert(IList<T> t);
+        #endregion
+
+      
+
+        #region 根据编号删除数据
+        /// <summary>
+        /// 根据编号删除数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        void Delete<Value>(int id);
+        #endregion
+
+        #region 根据Lambda表达式删除
+        /// <summary>
+        /// 根据Lambda表达式删除
+        /// </summary>
+        /// <param name="whereLambda"></param>
+        /// <returns></returns>
+        void Delete(Expression<Func<T, bool>> whereLambda);
+        #endregion
+
+      
+
+        #region 批量修改
+        /// <summary>
+        /// 批量修改
+        /// </summary>
+        /// <param name="entities"></param>
+        /// <returns></returns>
+        void Update(IList<T> entities);
+        #endregion
+
+       
+
+        #region 根据主键进行查询
+        /// <summary>
+        /// 根据主键进行查询
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        T FindByID(int Id);
+        #endregion
+
+        #region 默认查询选择第一条数据，没有那么返回null
+        /// <summary>
+        /// 默认查询选择第一条数据，没有那么返回null
+        /// </summary>
+        /// <param name="whereLambda"></param>
+        /// <returns></returns>
+        T GetFirstDefault(Expression<Func<T, bool>> whereLambda = null);
+        #endregion
+
+        #region 查询所有的数据
+        /// <summary>
+        /// 查询所有的数据
+        /// </summary>
+        /// <param name="whereLambda"></param>
+        /// <returns></returns>
+        IQueryable<T> GetAll(string whereLambda = null);
+        #endregion
+
+       
+
+        #region  获取数量
+        /// <summary>
+        /// 获取数量
+        /// </summary>
+        /// <param name="whereLambda"></param>
+        /// <returns></returns>
+        int GetCount(Expression<Func<T, bool>> whereLambda = null);
+        #endregion
+
+
+
+        #region 执行sql
+        /// <summary>
+        /// 执行sql
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="dyParmaters"></param>
+        /// <returns></returns>
+        void ExecuteSQL(string sql, object dyParmaters = null);
+        #endregion
+
+        #region 执行查询自定义对象集合
+        /// <summary>
+        /// 执行查询自定义对象集合
+        /// </summary>
+        /// <typeparam name="Value"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="dyParmaters"></param>
+        /// <returns></returns>
+        IQueryable<Value> Reader<Value>(string sql, object dyParmaters = null);
+        #endregion
+         
+        IQueryable<Value> ReaderSingle<Value>(string sql, object dyParmaters = null);
+
+       
+       
+
+        #region 执行SQL 语句返回一行一列值
+        /// <summary>
+        /// 执行SQL 语句返回一行一列值
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="dyParmaters"></param>
+        /// <returns></returns>
+        Value ExecuteScalar<Value>(string sql, object dyParmaters = null);
+        #endregion
     }
 }
--- Repository/RepositoryBase.cs
+++ Repository/RepositoryBase.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
+using System.Threading.Tasks;
 using Contracts;
+using EFCore.BulkExtensions;
 using Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -29,10 +31,6 @@ namespace Repository
             return this.RepositoryContext.Set<T>().Where(expression).AsNoTracking();
         }
 
-        public void Create(T entity)
-        {
-            this.RepositoryContext.Set<T>().Add(entity);
-        }
 
         public void Update(T entity)
         {
@@ -42,6 +40,72 @@ namespace Repository
         public void Delete(T entity)
         {
             this.RepositoryContext.Set<T>().Remove(entity);
+        }
+
+        void IRepositoryBase<T>.Insert(T t)
+        {
+            this.RepositoryContext.Set<T>().Add(t);
+        }
+
+        public void Insert(IList<T> t)
+        {
+            RepositoryContext.BulkInsert(t);
+        }
+
+        public void Delete<Value>(int id)
+        {
+            var model = RepositoryContext.Set<T>().Find(id);
+            RepositoryContext.Set<T>().Remove(model);
+        }
+
+        public void Delete(Expression<Func<T, bool>> whereLambda)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(IList<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindByID(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirstDefault(Expression<Func<T, bool>> whereLambda = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetAll(string whereLambda = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCount(Expression<Func<T, bool>> whereLambda = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExecuteSQL(string sql, object dyParmaters = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Value> Reader<Value>(string sql, object dyParmaters = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Value> ReaderSingle<Value>(string sql, object dyParmaters = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Value ExecuteScalar<Value>(string sql, object dyParmaters = null)
+        {
+            throw new NotImplementedException();
         }
     }
 }
